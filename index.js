8a17184ca90a561f0935d8e3f07edf842d8240bb
Florence <stamandf@gmail.com>
2021-10-10 11:39:45 -0400

    Change PORT definition and Add Procfile for Heroku.

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ let notes = [
     response.json(note)
   })
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
